3ce9995a99b8e7bf718e407e8d237753e186be4c
jonatas1096 <jonatas1096@hotmail.com>
2023-08-07 20:42:20 -0300

    teste ETEC

--- app/src/main/java/com/example/agendadecontatos/views/listaContatos.kt
+++ app/src/main/java/com/example/agendadecontatos/views/listaContatos.kt
@@ -2,17 +2,23 @@ package com.example.agendadecontatos.views
 
 import android.annotation.SuppressLint
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.FloatingActionButton
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.constraintlayout.compose.ConstraintLayout
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
 import com.example.agendadecontatos.R
@@ -20,27 +26,42 @@ import com.example.agendadecontatos.R
 @SuppressLint("UnusedMaterialScaffoldPaddingParameter")
 @Composable
 fun listaContatos(navController : NavController){
-    
-    Scaffold(
-        topBar = {
-            TopAppBar(
-                title = {
-                    Text(text = "Contatos",
-                        fontWeight = FontWeight.Bold,
-                        fontSize = 24.sp,
-                        )
-                },
-                backgroundColor = Color(0xFF01A0A0),
-                contentColor = Color.White,
-            )
-        }
-    ) {
-        FloatingActionButton(onClick = {
 
-        }) {
-            Image(imageVector = R.drawable.ic_launcher_background, contentDescription = "a")
+    Column(
+
+    ) {
+        Scaffold(
+            topBar = {
+                TopAppBar(
+                    title = {
+                        Text(text = "Contatos",
+                            fontWeight = FontWeight.Bold,
+                            fontSize = 24.sp,
+                        )
+                    },
+                    backgroundColor = Color(0xFF01A0A0),
+                    contentColor = Color.White,
+                )
+            }
+        ) {
+            ConstraintLayout() {
+                val (novocontatobotao)= createRefs()
+
+            }
+            FloatingActionButton(onClick = {
+
+            }, //to tentando adicionar um modifier aqui p usar parent nele
+                backgroundColor = Color(0xFF01A0A0),
+
+                ) {
+                Image(imageVector = ImageVector.vectorResource(id = R.drawable.ic_novocontato),
+                    contentDescription = "a",
+                    modifier = Modifier.size(30.dp)
+                )
+            }
         }
     }
+
 }
 
 @Preview(showBackground = true)
